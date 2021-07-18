f183e6dca377f1b058dbfe3c5e999b965f389315
nitsancohen770 <nitsancohen770@gmail.com>
2021-07-18 21:36:35 +0300

    fixed geology position

--- src/components/PlanetViewer/PlanetViewer.js
+++ src/components/PlanetViewer/PlanetViewer.js
@@ -1,5 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
+import { useLocation } from "@reach/router"
 
 const PlanetViewer = ({
   planetImages,
@@ -7,6 +8,8 @@ const PlanetViewer = ({
   geologyImages,
   currentView,
 }) => {
+  const location = useLocation()
+  console.log(location.pathname)
   const planetSVG = [planetImages, internalImages, geologyImages]
   return (
     <div style={{ display: "inline", marginTop: "70px" }}>
@@ -15,13 +18,23 @@ const PlanetViewer = ({
           {" "}
           <img src={planetSVG[0][0].publicURL} alt={planetSVG[0][0].name} />
           <img
-            style={{
-              position: "absolute",
-              left: "220px",
-              bottom: "-60px",
-              width: "200px",
-              height: "250px",
-            }}
+            style={
+              location.pathname !== "/saturn" || location.path === "/jupiter"
+                ? {
+                    position: "absolute",
+                    left: "25%",
+                    bottom: "-40%",
+                    width: "200px",
+                    height: "250px",
+                  }
+                : {
+                    position: "absolute",
+                    left: "35%",
+                    bottom: "-10%",
+                    width: "200px",
+                    height: "250px",
+                  }
+            }
             src={planetSVG[currentView][0].publicURL}
             alt={planetSVG[currentView][0].name}
           />
